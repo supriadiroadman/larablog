8f3d5edc4734d080420a2eb601722642bb69e459
supriadiroadman <36745375+supriadiroadman@users.noreply.github.com>
2021-05-22 08:04:18 +0700

    Middleware admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
     public function index(Request $request)
     {
-        // Jika role bukan admin
-        if (!auth()->user()->isAdmin()) {
-            return redirect()->route('profiles.index')->with('error', "Anda tidak memiliki akses");
-        }
-
         $keyword = $request->keyword;
 
         if ($keyword) {
@@ -67,10 +67,6 @@ class UserController extends Controller
 
     public function makeAdmin(User $user)
     {
-        if (!auth()->user()->isAdmin()) {
-            return redirect()->back()->with('error', "Anda tidak memiliki akses");
-        }
-
         $user->role = 'admin';
         $user->save();
 
@@ -78,11 +74,6 @@ class UserController extends Controller
     }
     public function makeUser(User $user)
     {
-        // Jika role bukan admin
-        if (!auth()->user()->isAdmin()) {
-            return redirect()->back()->with('error', "Anda tidak memiliki akses");
-        }
-
         // Jika role yang login= admin dan user id =1 tidak boleh khusus utk id ini
         if (auth()->user()->isAdmin() && $user->id == '1') {
             return redirect()->back()->with('error', "Tidak di izinkan");
---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()->isAdmin()) {
+            return $next($request);
+        }
+
+        return redirect()->route('profiles.index')->with('error', "Anda tidak memiliki akses");
+    }
+}
